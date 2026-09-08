0e51c86fe650bce98f441224ff81b1cff7684049
TRIXX-06 <sultantri@students.usu.ac.id>
2026-09-08 11:41:45 +0700

    pertemuan4

---
+++ pertemuan4/pertemuan4/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblNilaiAwal = New Label()
+        lblNilaiAkhir = New Label()
+        btnInput = New Button()
+        txtNilaiAwal = New TextBox()
+        txtNilaiAkhir = New TextBox()
+        lstHasil = New ListBox()
+        SuspendLayout()
+        ' 
+        ' lblNilaiAwal
+        ' 
+        lblNilaiAwal.AutoSize = True
+        lblNilaiAwal.Location = New Point(216, 46)
+        lblNilaiAwal.Name = "lblNilaiAwal"
+        lblNilaiAwal.Size = New Size(98, 25)
+        lblNilaiAwal.TabIndex = 0
+        lblNilaiAwal.Text = "Nilai Awal :"
+        ' 
+        ' lblNilaiAkhir
+        ' 
+        lblNilaiAkhir.AutoSize = True
+        lblNilaiAkhir.Location = New Point(216, 122)
+        lblNilaiAkhir.Name = "lblNilaiAkhir"
+        lblNilaiAkhir.Size = New Size(101, 25)
+        lblNilaiAkhir.TabIndex = 1
+        lblNilaiAkhir.Text = "Nilai Akhir :"
+        ' 
+        ' btnInput
+        ' 
+        btnInput.Location = New Point(236, 215)
+        btnInput.Name = "btnInput"
+        btnInput.Size = New Size(112, 34)
+        btnInput.TabIndex = 2
+        btnInput.Text = "Input"
+        btnInput.UseVisualStyleBackColor = True
+        ' 
+        ' txtNilaiAwal
+        ' 
+        txtNilaiAwal.Location = New Point(216, 74)
+        txtNilaiAwal.Name = "txtNilaiAwal"
+        txtNilaiAwal.Size = New Size(150, 31)
+        txtNilaiAwal.TabIndex = 3
+        ' 
+        ' txtNilaiAkhir
+        ' 
+        txtNilaiAkhir.Location = New Point(216, 150)
+        txtNilaiAkhir.Name = "txtNilaiAkhir"
+        txtNilaiAkhir.Size = New Size(150, 31)
+        txtNilaiAkhir.TabIndex = 4
+        ' 
+        ' lstHasil
+        ' 
+        lstHasil.FormattingEnabled = True
+        lstHasil.Location = New Point(25, 287)
+        lstHasil.Name = "lstHasil"
+        lstHasil.Size = New Size(527, 254)
+        lstHasil.TabIndex = 5
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(588, 651)
+        Controls.Add(lstHasil)
+        Controls.Add(txtNilaiAkhir)
+        Controls.Add(txtNilaiAwal)
+        Controls.Add(btnInput)
+        Controls.Add(lblNilaiAkhir)
+        Controls.Add(lblNilaiAwal)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblNilaiAwal As Label
+    Friend WithEvents lblNilaiAkhir As Label
+    Friend WithEvents btnInput As Button
+    Friend WithEvents txtNilaiAwal As TextBox
+    Friend WithEvents txtNilaiAkhir As TextBox
+    Friend WithEvents lstHasil As ListBox
+
+End Class
---
+++ pertemuan4/pertemuan4/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblNilaiAwal.Click
+
+    End Sub
+
+    Private Sub txtNilaiAwal_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNilaiAwal.KeyPress
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtNilaiAkhir_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNilaiAkhir.KeyPress
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub btnInput_Click(sender As Object, e As EventArgs) Handles btnInput.Click
+        Dim lblNilaiAwal As Integer
+        Dim lblNilaiAkhir As Integer
+
+        If Not Integer.TryParse(txtNilaiAwal.Text, lblNilaiAwal) Then
+            MessageBox.Show("Masukkan nilai dalam bentuk angka.")
+            Return
+        End If
+
+        If Not Integer.TryParse(txtNilaiAkhir.Text, lblNilaiAkhir) Then
+            MessageBox.Show("Masukkan nilai dalam bentuk angka.")
+            Return
+        End If
+
+        lstHasil.Items.Clear()
+        For i As Integer = lblNilaiAwal To lblNilaiAkhir Step 2
+            lstHasil.Items.Add(i)
+        Next
+    End Sub
+End Class
